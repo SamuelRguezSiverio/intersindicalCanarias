58f63dc16766b22d59ee9f0f8a33bc0eba54e8d2
SamuelRguezSiverio <samuelsrs89@gmail.com>
2024-02-28 19:56:57 +0000

    Working on Estatutaria

--- frontend/src/components/Estatutaria/Estatutaria.jsx
+++ frontend/src/components/Estatutaria/Estatutaria.jsx
@@ -1,10 +1,234 @@
 
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Estatutaria.css';
 
-function Estatutaria() {
+const Estatutaria = () => {
+  const categoriasLaborales = [
+    "albañil",
+    "alergologia",
+    "analisisClinicos",
+    "anatomiaPatologica",
+    "angiologiaCirugiaVascular",
+    "aparatoDigestivo",
+    "auxiliarAdministrativoFuncionAdministrativa",
+    "auxiliarEnfermeria",
+    "bioquimicaClinica",
+    "calefactor",
+    "cardiologia",
+    "carpintero",
+    "celador",
+    "cirugiaCardiovascular",
+    "cirugiaGeneralAparatoDigestivo",
+    "cirugiaOralMaxilofacial",
+    "cirugiaOrtopedicaTraumatologica",
+    "cirugiaPediatrica",
+    "cirugiaPlasticaEsteticaReparadora",
+    "cirugiaToracica",
+    "cocinero",
+    "conductor",
+    "conductorInstalaciones",
+    "dermatologiaMedicoQuirurgicaVenereologia",
+    "electricista",
+    "endocrinologiaNutricion",
+    "enfermera",
+    "farmacologiaClinica",
+    "fisioterapeuta",
+    "fontanero",
+    "geriatria",
+    "gobernanta",
+    "grupoAdministrativoFuncionAdministrativa",
+    "hematologiaHemoterapia",
+    "ingenieroTecnico",
+    "ingenieroTecnicoIndustrial",
+    "lavandera",
+    "limpiador",
+    "logopeda",
+    "matrona",
+    "mecanico",
+    "medicinaDelTrabajo",
+    "medicinaFamiliarComunitaria",
+    "medicinaFisicaRehabilitacion",
+    "medicinaIntensiva",
+    "medicinaInterna",
+    "medicinaNuclear",
+    "medicinaPreventivaSaludPublica",
+    "medicoAdmisionDocumentacionClinica",
+    "medicoFamilia",
+    "medicoUrgenciaHospitalaria",
+    "microbiologiaParasitologia",
+    "nefrologia",
+    "neumologia",
+    "neurocirugia",
+    "neurofisiologiaClinica",
+    "neurologia",
+    "obstetriciaGinecologia",
+    "odontestomatologa",
+    "oftalmologia",
+    "oncologiaMedica",
+    "oncologiaRadioterapica",
+    "pediatraEquipoAtencionPrimaria",
+    "pediatriaAreasEspecificas",
+    "peon",
+    "pinche",
+    "pintor",
+    "planchadora",
+    "psicologiaClinica",
+    "psiquiatria",
+    "radiodiagnostico",
+    "radiofisicaHospitalaria",
+    "reumatologia",
+    "tecnicoEspecialistaAnatomiaPatologica",
+    "tecnicoEspecialistaDocumentacionSanitaria",
+    "tecnicoEspecialistaHigieneBucodental",
+    "tecnicoEspecialistaLaboratorio",
+    "tecnicoEspecialistaMedicinaNuclear",
+    "tecnicoEspecialistaRadiodiagnostico",
+    "tecnicoEspecialistaRadioterapia",
+    "tecnicoSaludPublica",
+    "tecnicoSuperiorGestionServicios",
+    "tecnicoTituladoMedioInformatica",
+    "tecnicoTituladoSuperiorADE",
+    "tecnicoTituladoSuperiorEconomico",
+    "tecnicoTituladoSuperiorInformatica",
+    "telefonista",
+    "terapeutaOcupacional",
+    "trabajadorSocial",
+    "urologia"
+  ];
+  
+
+  const [busqueda, setBusqueda] = useState('');
+
+  const categoriasFiltradas =
+    busqueda.length === 0
+      ? categoriasLaborales
+      : categoriasLaborales.filter((categoria) =>
+          categoria.toLowerCase().includes(busqueda.toLowerCase())
+        );
+
+  // Función para formatear el nombre de la categoría para la visualización
+  const formatearNombreCategoria = (categoria) => {
+const formatos = {
+  "albañil": "Albañil",
+  "alergologia": "Alergología",
+  "analisisClinicos": "Análisis Clínicos",
+  "anatomiaPatologica": "Anatomía Patológica",
+  "angiologiaCirugiaVascular": "Angiología y Cirugía Vascular",
+  "aparatoDigestivo": "Aparato Digestivo",
+  "auxiliarAdministrativoFuncionAdministrativa": "Auxiliar Administrativo Función Administrativa",
+  "auxiliarEnfermeria": "Auxiliar Enfermería",
+  "bioquimicaClinica": "Bioquímica Clínica",
+  "calefactor": "Calefactor",
+  "cardiologia": "Cardiología",
+  "carpintero": "Carpintero",
+  "celador": "Celador",
+  "cirugiaCardiovascular": "Cirugía Cardiovascular",
+  "cirugiaGeneralAparatoDigestivo": "Cirugía General y Aparato Digestivo",
+  "cirugiaOralMaxilofacial": "Cirugía Oral y Maxilofacial",
+  "cirugiaOrtopedicaTraumatologica": "Cirugía Ortopédica y Traumatológica",
+  "cirugiaPediatrica": "Cirugía Pediátrica",
+  "cirugiaPlasticaEsteticaReparadora": "Cirugía Plástica, Estética y Reparadora",
+  "cirugiaToracica": "Cirugía Torácica",
+  "cocinero": "Cocinero/a",
+  "conductor": "Conductor/a",
+  "conductorInstalaciones": "Conductor/a de Instalaciones",
+  "dermatologiaMedicoQuirurgicaVenereologia": "Dermatología Médico-Quirúrgica y Venereología",
+  "electricista": "Electricista",
+  "endocrinologiaNutricion": "Endocrinología y Nutrición",
+  "enfermera": "Enfermero/a",
+  "farmacologiaClinica": "Farmacología Clínica",
+  "fisioterapeuta": "Fisioterapeuta",
+  "fontanero": "Fontanero",
+  "geriatria": "Geriatría",
+  "gobernanta": "Gobernanta",
+  "grupoAdministrativoFuncionAdministrativa": "Grupo Administrativo Función Administrativa",
+  "hematologiaHemoterapia": "Hematología y Hemoterapia",
+  "ingenieroTecnico": "Ingeniero Técnico",
+  "ingenieroTecnicoIndustrial": "Ingeniero Técnico Industrial",
+  "lavandera": "Lavandera",
+  "limpiador": "Limpiador/a",
+  "logopeda": "Logopeda",
+  "matrona": "Matrona",
+  "mecanico": "Mecánico",
+  "medicinaDelTrabajo": "Medicina del Trabajo",
+  "medicinaFamiliarComunitaria": "Medicina Familiar y Comunitaria",
+  "medicinaFisicaRehabilitacion": "Medicina Física y Rehabilitación",
+  "medicinaIntensiva": "Medicina Intensiva",
+  "medicinaInterna": "Medicina Interna",
+  "medicinaNuclear": "Medicina Nuclear",
+  "medicinaPreventivaSaludPublica": "Medicina Preventiva y Salud Pública",
+  "medicoAdmisionDocumentacionClinica": "Médico de Admisión y Documentación Clínica",
+  "medicoFamilia": "Médico de Familia",
+  "medicoUrgenciaHospitalaria": "Médico de Urgencia Hospitalaria",
+  "microbiologiaParasitologia": "Microbiología y Parasitología",
+  "nefrologia": "Nefrología",
+  "neumologia": "Neumología",
+  "neurocirugia": "Neurocirugía",
+  "neurofisiologiaClinica": "Neurofisiología Clínica",
+  "neurologia": "Neurología",
+  "obstetriciaGinecologia": "Obstetricia y Ginecología",
+  "odontestomatologa": "Odontestomatología",
+  "oftalmologia": "Oftalmología",
+  "oncologiaMedica": "Oncología Médica",
+  "oncologiaRadioterapica": "Oncología Radioterápica",
+  "pediatraEquipoAtencionPrimaria": "Pediatra de Equipo de Atención Primaria",
+  "pediatriaAreasEspecificas": "Pediatría de Áreas Específicas",
+  "peon": "Peón",
+  "pinche": "Pinche",
+  "pintor": "Pintor",
+  "planchadora": "Planchadora",
+  "psicologiaClinica": "Psicología Clínica",
+  "psiquiatria": "Psiquiatría",
+  "radiodiagnostico": "Radiodiagnóstico",
+  "radiofisicaHospitalaria": "Radiofísica Hospitalaria",
+  "reumatologia": "Reumatología",
+  "tecnicoEspecialistaAnatomiaPatologica": "Técnico Especialista en Anatomía Patológica",
+  "tecnicoEspecialistaDocumentacionSanitaria": "Técnico Especialista en Documentación Sanitaria",
+  "tecnicoEspecialistaHigieneBucodental": "Técnico Especialista en Higiene Bucodental",
+  "tecnicoEspecialistaLaboratorio": "Técnico Especialista de Laboratorio",
+  "tecnicoEspecialistaMedicinaNuclear": "Técnico Especialista en Medicina Nuclear",
+  "tecnicoEspecialistaRadiodiagnostico": "Técnico Especialista en Radiodiagnóstico",
+  "tecnicoEspecialistaRadioterapia": "Técnico Especialista en Radioterapia",
+  "tecnicoSaludPublica": "Técnico de Salud Pública",
+  "tecnicoSuperiorGestionServicios": "Técnico Superior en Gestión de Servicios",
+  "tecnicoTituladoMedioInformatica": "Técnico Titulado Medio en Informática",
+  "tecnicoTituladoSuperiorADE": "Técnico Titulado Superior en ADE",
+  "tecnicoTituladoSuperiorEconomico": "Técnico Titulado Superior en Económico",
+  "tecnicoTituladoSuperiorInformatica": "Técnico Titulado Superior en Informática",
+  "telefonista": "Telefonista",
+  "terapeutaOcupacional": "Terapeuta Ocupacional",
+  "trabajadorSocial": "Trabajador Social",
+  "urologia": "Urología"
+};
+
+    return formatos[categoria] || categoria;
+  };
+
   return (
-    <div>Estatutaria</div>
-  )
-}
+    <div className="estatutaria-container">
+      <h2>Categorías Laborales</h2>
+      <input
+        type="text"
+        placeholder="Buscar categoría..."
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+        className="search-input"
+      />
 
-export default Estatutaria
+      <div className="categories-container">
+        {categoriasFiltradas.map((categoria, index) => (
+          <Link
+            key={index}
+            to={`/estatutaria/categoria/${categoria}`}
+            className="animated-button"
+          >
+            {formatearNombreCategoria(categoria)}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Estatutaria;
--- frontend/src/components/TestExamen/TestExamen.jsx
+++ frontend/src/components/TestExamen/TestExamen.jsx
@@ -1,103 +1,105 @@
 // TestExamen.js
-import React, { useState, useEffect } from 'react';
-import { useLoaderData, useNavigate } from 'react-router-dom';
-import './TestExamen.css';
+import React, { useState, useEffect } from 'react'
+import { useLoaderData, useNavigate } from 'react-router-dom'
+import './TestExamen.css'
 
 // Función para mezclar las preguntas
 const mezclarPreguntas = (array) => {
   let currentIndex = array.length,
     temporaryValue,
-    randomIndex;
+    randomIndex
 
   // Mientras queden elementos a mezclar...
   while (0 !== currentIndex) {
     // Seleccionar un elemento sin mezclar...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
 
     // E intercambiarlo con el elemento actual
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
   }
 
-  return array;
-};
+  return array
+}
 
 const TestExamen = () => {
-  const { preguntas } = useLoaderData();
-  const [preguntasMezcladas, setPreguntasMezcladas] = useState([]);
-  const [indiceActual, setIndiceActual] = useState(0);
-  const [respuestaSeleccionada, setRespuestaSeleccionada] = useState(null);
-  const [temporizador, setTemporizador] = useState(null);
-  const [finalizado, setFinalizado] = useState(false);
-  const navigate = useNavigate();
+  const { preguntas } = useLoaderData()
+  const [preguntasMezcladas, setPreguntasMezcladas] = useState([])
+  const [indiceActual, setIndiceActual] = useState(0)
+  const [respuestaSeleccionada, setRespuestaSeleccionada] = useState(null)
+  const [temporizador, setTemporizador] = useState(null)
+  const [finalizado, setFinalizado] = useState(false)
+  const navigate = useNavigate()
 
   // Configuración basada en el grupo de la categoría
   const configuracionExamen = {
-    'A1': { maxPreguntas: 100, tiempoMax: 120 },
-    'A2': { maxPreguntas: 90, tiempoMax: 120 },
-    'default': { maxPreguntas: 50, tiempoMax: 90 }
-  };
+    A1: { maxPreguntas: 100, tiempoMax: 120 },
+    A2: { maxPreguntas: 90, tiempoMax: 120 },
+    default: { maxPreguntas: 50, tiempoMax: 90 },
+  }
 
   // Establecer configuración inicial basada en el grupo de la categoría
   useEffect(() => {
     if (preguntas && preguntas.length > 0) {
       // Asumimos que todas las preguntas tienen el mismo grupo
-      const grupoCategoria = preguntas[0].group;
-      const { maxPreguntas, tiempoMax } = configuracionExamen[grupoCategoria] || configuracionExamen['default'];
-      setPreguntasMezcladas(mezclarPreguntas([...preguntas]).slice(0, maxPreguntas));
+      const grupoCategoria = preguntas[0].group
+      const { maxPreguntas, tiempoMax } =
+        configuracionExamen[grupoCategoria] || configuracionExamen['default']
+      setPreguntasMezcladas(
+        mezclarPreguntas([...preguntas]).slice(0, maxPreguntas)
+      )
       // Solo establece el temporizador si aún no se ha establecido
       if (temporizador === null) {
-        setTemporizador(tiempoMax * 60); // Convertir minutos a segundos
+        setTemporizador(tiempoMax * 60) // Convertir minutos a segundos
       }
     }
     // Asegúrate de que 'configuracionExamen' y 'temporizador' no cambien en cada renderización,
     // de lo contrario, crea una referencia constante fuera del componente o usa useMemo.
-  }, [preguntas]);
-  
+  }, [preguntas])
 
   useEffect(() => {
     if (!finalizado) {
       const timer = setTimeout(() => {
         if (temporizador > 0) {
-          setTemporizador(temporizador - 1);
+          setTemporizador(temporizador - 1)
         } else {
-          setFinalizado(true);
+          setFinalizado(true)
         }
-      }, 1000);
+      }, 1000)
 
-      return () => clearTimeout(timer);
+      return () => clearTimeout(timer)
     }
-  }, [temporizador, finalizado]);
+  }, [temporizador, finalizado])
 
   const manejarRespuesta = (respuesta) => {
-    setRespuestaSeleccionada(respuesta);
-  };
-
-  const siguientePregunta = () => {
-    const siguienteIndice = indiceActual + 1;
-    if (siguienteIndice < preguntasMezcladas.length) {
-      setIndiceActual(siguienteIndice);
-      setRespuestaSeleccionada(null);
-    } else {
-      setFinalizado(true);
-    }
-  };
-
-  const reiniciarExamen = () => {
-    navigate(-1); // Vuelve a la pantalla anterior
-  };
-
-  if (!preguntasMezcladas || preguntasMezcladas.length === 0) {
-    return <div>Cargando preguntas...</div>;
+    setRespuestaSeleccionada(respuesta)
   }
 
-  const preguntaActual = preguntasMezcladas[indiceActual];
+  const siguientePregunta = () => {
+    const siguienteIndice = indiceActual + 1
+    if (siguienteIndice < preguntasMezcladas.length) {
+      setIndiceActual(siguienteIndice)
+      setRespuestaSeleccionada(null)
+    } else {
+      setFinalizado(true)
+    }
+  }
+
+  const reiniciarExamen = () => {
+    navigate(-1) // Vuelve a la pantalla anterior
+  }
+
+  if (!preguntasMezcladas || preguntasMezcladas.length === 0) {
+    return <div>Cargando preguntas...</div>
+  }
+
+  const preguntaActual = preguntasMezcladas[indiceActual]
   const opciones = [
     preguntaActual.correct_answer,
     ...preguntaActual.incorrect_answers,
-  ];
+  ]
 
   return (
     <div className="test-container">
@@ -113,7 +115,10 @@ const TestExamen = () => {
               <button onClick={reiniciarExamen} className="test-reiniciar-btn">
                 Reiniciar
               </button>
-              <p className="test-timer">Tiempo restante: {Math.floor(temporizador / 60)}:{('0' + temporizador % 60).slice(-2)}</p>
+              <p className="test-timer">
+                Tiempo restante: {Math.floor(temporizador / 60)}:
+                {('0' + (temporizador % 60)).slice(-2)}
+              </p>
             </div>
             <div>
               <p className="test-question">{preguntaActual.question}</p>
@@ -147,7 +152,7 @@ const TestExamen = () => {
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TestExamen;
+export default TestExamen
