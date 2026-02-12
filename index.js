ad8e800b94dab59ca69c75f99f1ea15d4504400e
SamuelRguezSiverio <samuelsrs89@gmail.com>
2026-02-12 21:24:15 +0000

    cambio en logs

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('./backend/index.js')
+/* index.js */
+console.log('--- INICIO DE LA APLICACIÓN ---');
+
+try {
+  require('./backend/index.js');
+} catch (error) {
+  console.error('--- ERROR FATAL AL INICIAR ---');
+  console.error(error);
+}
