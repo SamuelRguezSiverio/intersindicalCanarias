a91a58c8ae698967d3276c722ca9a5896f34898a
SamuelRguezSiverio <samuelsrs89@gmail.com>
2026-02-12 21:33:31 +0000

    database index changed

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
-  host: process.env.HOST,
-  dialect: process.env.DIALECT,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'mysql', 
+  port: process.env.DB_PORT || 3306,
+  logging: false,
   dialectOptions: {
     charset: 'utf8mb4',
     collate: 'utf8mb4_unicode_ci'
